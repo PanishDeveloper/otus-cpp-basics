9e0c4918feae4e975ccb94f98e709217518a1ec4
Panish <panish20091995@gmail.com>
2026-01-30 19:46:33 +0300

    Homework 3: Basic game with menu

---
+++ homework3/build/CMakeFiles/gues_the_number.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gues_the_number.dir/game.cpp.o"
+  "CMakeFiles/gues_the_number.dir/game.cpp.o.d"
+  "CMakeFiles/gues_the_number.dir/main.cpp.o"
+  "CMakeFiles/gues_the_number.dir/main.cpp.o.d"
+  "gues_the_number"
+  "gues_the_number.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gues_the_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
